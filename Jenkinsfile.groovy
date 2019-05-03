b7928f8930a4d207e4d34934fed46ae31dde9a72
addiani <addiani744@gmail.com>
2019-05-02 19:54:48 -0500

    added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,4 +3,7 @@ node{
         git 'git@github.com:addiani/cool_website.git'
        
     }
+    stage("webserver Install"){
+        sh "ssh ec2-user@18.188.162.93       sudo yum install httpd -y"
+    }
 }
